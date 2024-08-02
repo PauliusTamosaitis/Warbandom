5d51cfe7155eeea1a9cd0a8cce9a4d60270a011b
PauliusTamosaitis <paulius.tamosaitis@gmail.com>
2024-08-02 14:20:25 +0300

    Create WarII.py

---
+++ WarII.py
@@ -0,0 +1,33 @@
+import random
+
+class Card:
+    def __init__(self, rank, suit):
+        self.rank = rank
+        self.suit = suit
+
+    def __str__(self):
+        rank_name = self.rank
+        if self.rank == 11:
+            rank_name = "Jack"
+        elif self.rank == 12:
+            rank_name = "Queen"
+        elif self.rank == 13:
+            rank_name = "King"
+        elif self.rank == 14:
+            rank_name = "Ace"
+        return f"{rank_name} of {self.suit}"
+
+class Deck:
+    def __init__(self):
+        self.cards = []
+        self.build_deck()
+        random.shuffle(self.cards)
+
+    def build_deck(self):
+        for suit in ["Spades", "Hearts", "Clubs", "Diamonds"]:
+            for rank in range(2, 15):
+                self.cards.append(Card(rank, suit))
+
+    def draw_card(self):
+        return self.cards.pop(0)
+
