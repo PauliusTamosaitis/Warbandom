1bbe4732f79095645397a044eb72981373e124ac
PauliusTamosaitis <paulius.tamosaitis@gmail.com>
2024-08-02 14:31:36 +0300

    Update WarII.py

--- WarII.py
+++ WarII.py
@@ -30,4 +30,11 @@ class Deck:
 
     def draw_card(self):
         return self.cards.pop(0)
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.total_score = 0
+
+    def add_point(self):
+        self.total_score += 1
 
